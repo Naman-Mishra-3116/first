f9639ac9be5ac8ed0e94cd975d5be8d02595d283
Naman Mishra <namanmishra3116@gmail.com>
2023-07-29 21:59:09 +0530

    how is the josh

--- check.java
+++ check.java
@@ -2,5 +2,11 @@ public class check
 {
     public static void main(String[] args) {
         System.out.println("hello world, how is the josh");
+        System.out.println("how is the josh ");
+        int a;
+        int b;
+        int c;
+        
     }
+
 }
