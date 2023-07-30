c54c60eeac42b0270928c8e671bfd835ea33fdad
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2023-07-30 20:07:13 +0530

    this is first commit of the day

---
+++ first.java
@@ -0,0 +1,12 @@
+public class check
+{
+    public static void main(String[] args) {
+        System.out.println("hello world, how is the josh");
+        System.out.println("how is the josh ");
+        int a;
+        int b;
+        int c;
+        
+    }
+
+}
