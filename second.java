a38f56e3841133ab9e53acef5af8991ec78bec1b
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2023-07-30 20:25:05 +0530

    second file

---
+++ second.java
@@ -0,0 +1,10 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class second {
+ public static void main(String ... x)
+ {
+    System.out.println("this is the testing phase of github");
+ }   
+}
